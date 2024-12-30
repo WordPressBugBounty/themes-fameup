e7e91d9357a17c1dcd5030ef2eff41898e433cc8
Sean Murphy <sean@iamseanmurphy.com>
2024-12-30 06:18:16 +0000

    1.0.0.41

--- fameup/inc/ansar/hooks/hook-side-menu-section.php
+++ fameup/inc/ansar/hooks/hook-side-menu-section.php
@@ -16,9 +16,22 @@ function fameup_side_menu_section()
     <span class="btn_close" data-bs-dismiss="offcanvas" aria-label="Close"><i class="fas fa-times"></i></span>
   </div>
   <div class="offcanvas-body">
-      <?php if( is_active_sidebar('menu-sidebar-content')){
-    get_template_part('sidebar','fameupmenu');
-  } else { echo esc_html_e('No Widgets found in the Sidebar','fameup'); } ?>
+    <?php if( is_active_sidebar('menu-sidebar-content')){
+      get_template_part('sidebar','fameupmenu');
+    } else { ?>
+      <div class="bs-card-box empty-sidebar">
+        <div class="bs-widget-title one">
+          <h2 class='title'><?php esc_html_e( 'Menu Sidebar Widget Area', 'fameup' ); ?></h3>
+        </div>
+        <p class='empty-sidebar-widget-text'>
+          <?php esc_html_e( 'This is an example widget to show how the Menu Sidebar Widget Area looks by default. You can add custom widgets from the', 'fameup' ); ?>
+          <a href='<?php echo esc_url( admin_url( 'widgets.php' ) ); ?>' title='<?php esc_attr_e('widgets','fameup'); ?>'>
+            <?php esc_html_e( 'widgets', 'fameup' ); ?>
+          </a>
+          <?php esc_html_e( 'in the admin.', 'fameup' ); ?>
+        </p>
+      </div>
+    <?php } ?>
   </div>
 </div>
 <?php 
--- fameup/readme.txt
+++ fameup/readme.txt
@@ -4,8 +4,8 @@ Author: Themeansar
 Requires at least: WordPress 5.4
 Tested up to: WordPress 6.7
 Requires PHP: 7.4
-Stable tag: 1.0.0.40
-Version: 1.0.0.40
+Stable tag: 1.0.0.41
+Version: 1.0.0.41
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Tags: one-column, two-columns ,right-sidebar, flexible-header, custom-background, custom-header, custom-menu, editor-style, featured-images, footer-widgets,  theme-options, threaded-comments, rtl-language-support, translation-ready, full-width-template, custom-logo, blog, news
@@ -393,3 +393,6 @@ Source: https://pxhere.com/en/photo/1336736
 
 = Version 1.0.0.40
 * Fixed RTL Styling Issue.
+
+= Version 1.0.0.41
+* Added Default Widget in Menu Sidebar Widget Area.
